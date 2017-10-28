5526256e2ab13b246f44d7a154e3516ab017485c
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-10-28 17:56:10 +0200

    Server NodeJS for connection and deconnection users to capture the presence or absence of a user

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,2 +1,34 @@
 require('./bootstrap');
 
+import io from 'socket.io-client'
+
+let presence = document.querySelector('#presence')
+
+let addUser = function (user) {
+    let li = document.createElement('li')
+    li.innerText = user.name
+    li.id = 'user' + user.id
+    presence.appendChild(li)
+};
+
+if (presence) {
+    let socket = io('http://localhost:3000')
+
+    socket.on('connect', _ => {
+        socket.emit('identify', {
+            token: presence.dataset.token
+        })
+    })
+
+    socket.on('users.new', ({user}) => {
+        addUser(user);
+    })
+
+    socket.on('users.leave', ({user}) => {
+        presence.removeChild(document.querySelector('#user' + user.id))
+    })
+
+    socket.on('users', ({users}) => {
+
+    })
+}
---
+++ websocket/server.js
@@ -0,0 +1,59 @@
+/**
+ * Created by amacabr2 on 28/10/17.
+ */
+
+// 27.55
+
+let io = require('socket.io')(3000)
+let jwt = require('jsonwebtoken')
+
+let users = []
+
+let findCurrentUser = function (currentUser) {
+    return users.find(u => u.id === currentUser.id);
+};
+
+io.on('connection', socket => {
+
+    let currentUser = null
+
+    socket.on('identify', ({token}) => {
+        try {
+            let decoded = jwt.verify(token, 'demo', {
+                algorithms: ['HS256']
+            })
+
+            currentUser = {
+                id: decoded.user_id,
+                name: decoded.user_name,
+                count: 1
+            }
+
+            let user = findCurrentUser(currentUser)
+
+            if (user) {
+               user.count++
+            } else {
+                users.push(currentUser)
+                socket.broadcast.emit('users.new', {user: currentUser})
+            }
+
+            socket.emit('users', {users})
+        } catch (e) {
+            console.log(e)
+        }
+    })
+
+    socket.on('disconnect', _ => {
+        if (currentUser) {
+            let user = findCurrentUser(currentUser)
+            if (user) {
+                user.count--
+                if (user.count === 0) {
+                    users = users.filter(u => u.id !== currentUser.id)
+                    socket.broadcast.emit('users.leave', {user: currentUser})
+                }
+            }
+        }
+    })
+})
